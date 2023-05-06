c2a30ae36854a29a998a838ef7fb52a7f9fd38b1
dtakkiy <dtakkiy00@gmail.com>
2023-05-07 08:12:04 +0900

    remove recoil.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,10 @@
 import BottomTabNavigator from './lib/features/bottomTabNavigator';
 import './lib/core/firebase/firebase';
-import { RecoilRoot } from 'recoil';
 
 export default function App() {
   return (
-    <RecoilRoot>
+    <>
       <BottomTabNavigator />
-    </RecoilRoot>
+    </>
   );
 }
