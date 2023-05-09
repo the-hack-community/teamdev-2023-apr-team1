f69150875266c3a0db0b018cd0b3b196fa815cb2
sho <w.s.19891113@gmail.com>
2023-05-09 22:30:16 +0900

    backend initial commit

---
+++ backend/stray-cat-api/README.md
@@ -0,0 +1,44 @@
+# golangでAPIサーバを立ち上げるサンプル
+
+## 手順
+
+1. ".env"ファイル作成
+```
+DB_HOST=db
+DB_PORT=5432
+DB_USER=your_username
+DB_PASSWORD=your_password
+DB_NAME=sample_db
+```
+2. docker-compose実行
+
+```bash
+$ docker-compose up --build
+```
+
+
+3. 確認方法
+
+```bash
+# GET
+curl -X GET "http://localhost:8080/api/v1/users/"
+curl -X GET "http://localhost:8080/api/v1/users/1"
+
+# POST
+curl -X POST "http://localhost:8080/api/v1/users/" -H "Content-Type: application/json" -d '{"name": "John Doe", "email": "john.doe@example.com"}'
+
+# PUT
+curl -X PUT "http://localhost:8080/api/v1/users/1" -H "Content-Type: application/json" -d '{"name": "John Doe Updated", "email": "john.doe.updated@example.com"}'
+
+# DELETE
+curl -X DELETE "http://localhost:8080/api/v1/users/1"
+
+
+```
+
+
+1. 認証エンドポイント(メール認証の場合)
+   1. postmanにpostman.jsonをインポート
+   2. [認証エンドポイント]のPre-request Scriptの変数部分を修正
+      1. APIキー、メールアドレス、パスワード
+   3. Sendで確認
---
+++ backend/stray-cat-api/cmd/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"sample-api/db"
+	"sample-api/infrastructure/persistence/postgres"
+	"sample-api/presentation/handler"
+	"sample-api/usecase/interactor"
+	"strings"
+
+	"net/http"
+
+	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Read environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	// Create database connection string
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=%s port=%s", dbUser, dbPassword, dbName, dbHost, dbPort)
+
+	// Run database migrations
+	db.RunMigrations(connStr)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	userRepository := postgres.NewUserRepository(db)
+	userInteractor := interactor.UserInteractor{
+		UserRepository: userRepository,
+	}
+	userHandler := handler.UserHandler{
+		UserInteractor: userInteractor,
+	}
+
+	// Initialize Firebase
+	app := initFirebase()
+
+	r := gin.Default()
+
+	api := r.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			// 認証が不要なエンドポイント
+			users := v1.Group("/users")
+			{
+				users.GET("/", userHandler.GetAll)
+				users.GET("/:id", userHandler.GetByID)
+				users.POST("/", userHandler.Create)
+				users.PUT("/:id", userHandler.Update)
+				users.DELETE("/:id", userHandler.Delete)
+			}
+
+			// 認証が必要なエンドポイント
+			authUsers := v1.Group("/auth-users")
+			authUsers.Use(FirebaseAuthMiddleware(app))
+			{
+				authUsers.GET("/", userHandler.GetAll)
+				authUsers.GET("/:id", userHandler.GetByID)
+				authUsers.POST("/", userHandler.Create)
+				authUsers.PUT("/:id", userHandler.Update)
+				authUsers.DELETE("/:id", userHandler.Delete)
+			}
+		}
+	}
+
+	r.Run(":8080")
+}
+
+// Initialize Firebase
+func initFirebase() *firebase.App {
+	saKeyPath := "serviceAccountKey.json"
+	opt := option.WithCredentialsFile(saKeyPath)
+
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("Error initializing Firebase: %v", err)
+	}
+
+	return app
+}
+
+// FirebaseAuthMiddleware is a middleware for Firebase authentication
+func FirebaseAuthMiddleware(app *firebase.App) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		log.Printf(authHeader)
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header not found"})
+			c.Abort()
+			return
+		}
+
+		idToken := strings.TrimSpace(strings.Replace(authHeader, "Bearer", "", 1))
+
+		log.Printf(idToken)
+
+		client, err := app.Auth(context.Background())
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error initializing Auth client"})
+			c.Abort()
+			return
+		}
+
+		token, err := client.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid ID token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("uid", token.UID)
+
+		c.Next()
+	}
+}
