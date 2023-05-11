cad1aa7cb21201744e8c9feaafbc0730097b5e66
Yukihiro Shinoda <sinoda1114@gmail.com>
2023-05-12 02:44:48 +0900

    Image_optimize

--- frontend/lib/features/posts/page/index.tsx
+++ frontend/lib/features/posts/page/index.tsx
@@ -1,46 +1,80 @@
 import { NativeBaseProvider, ScrollView } from 'native-base';
-import React, { useEffect, useState } from 'react';
-import { Image, View } from 'react-native';
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
+import { Image, View, Button } from 'react-native'; // Buttonを追加
+import ImagePicker from 'react-native-image-picker';
 
-type jsonData = {
+// 画像データの型定義
+type CatImageData = {
   id: string;
   url: string;
-  width: string;
-  height: string;
+  width: number;
+  height: number;
 };
 
-// 画像表示のデモ
 function PostsScreen() {
-  const [catImages, setCatImages] = useState<jsonData[]>([]);
+  const [catImages, setCatImages] = useState<CatImageData[]>([]);
   const baseURL = 'https://api.thecatapi.com/v1/images/search';
 
-  const fetchData = async () => {
-    // 10件のデータを取得
+  // APIから画像データを取得する関数
+  const fetchData = useCallback(async () => {
     const json = await fetch(`${baseURL}?limit=10`);
     const data = await json.json();
     if (data) {
       setCatImages(data);
     }
-  };
+  }, []);
 
+  // APIから画像データを取得
   useEffect(() => {
     fetchData();
+  }, [fetchData]);
+
+  // 画像選択機能
+  const handleImagePicker = useCallback(() => {
+    const options = {
+      title: 'Select Avatar',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+    // 画像選択ダイアログを表示
+    ImagePicker.showImagePicker(options, response => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        const source = { uri: response.uri };
+        const resizedSource = {
+          uri: response.uri,
+          width: response.width,
+          height: response.height,
+        };
+        setCatImages(prevState => [ ...prevState, resizedSource ]);
+      }
+    });
   }, []);
+
+  // 画像データを表示する
+  const catImageData = useMemo(() => catImages.map((item, index) => (
+    <View key={index}>
+      <Image
+        style={{ width: item.width, height: item.height }} 
+        source={{ uri: item.url }}
+        alt="cat image"
+      />
+    </View>
+  )), [catImages]);
 
   return (
     <NativeBaseProvider>
       <ScrollView>
-        {catImages &&
-          catImages.map((item: jsonData, index: number) => {
-            return (
-              <View key={index}>
-                <Image
-                  style={{ width: 400, height: 400 }}
-                  source={{ uri: item.url }}
-                />
-              </View>
-            );
-          })}
+        {catImageData}
+        <View>
+          <Button onPress={handleImagePicker} title="Pick an image" />
+        </View>
       </ScrollView>
     </NativeBaseProvider>
   );
